38a4e30ddd02d480c36b7b150f28399c75bc6a42
ctrites1 <ctrites1@my.bcit.ca>
2025-04-25 09:09:28 -0700

    update cors

--- server/app.ts
+++ server/app.ts
@@ -1,20 +1,26 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { petsRoute } from "./routes/petRoutes";
-import { serveStatic } from "hono/bun";
-import { authRoute } from "./routes/auth";
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { petsRoute } from './routes/petRoutes';
+import { serveStatic } from 'hono/bun';
+import { authRoute } from './routes/auth';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
-app.use("*", logger());
+app.use('*', logger());
+app.use(
+  '/api/*',
+  cors({
+    origin: 'https://pet-health-tracker.fly.dev',
+    allowMethods: ['POST', 'GET'],
+    credentials: true,
+  }),
+);
 
-const apiRoutes = app
-	.basePath("/api")
-	.route("/pets", petsRoute)
-	.route("/", authRoute);
+const apiRoutes = app.basePath('/api').route('/pets', petsRoute).route('/', authRoute);
 
-app.get("*", serveStatic({ root: "./client/dist" }));
-app.get("*", serveStatic({ path: "./client/dist/index.html" }));
+app.get('*', serveStatic({ root: './client/dist' }));
+app.get('*', serveStatic({ path: './client/dist/index.html' }));
 
 export default app;
 export type ApiRoutes = typeof apiRoutes;
